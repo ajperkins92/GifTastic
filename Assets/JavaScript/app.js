371f71ef3d3a94ea1c71cbf972c90ff81541aa55
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-10-03 02:05:48 -0700

    Successfully created click events, rendered buttons to page, queried API on click events, and changed state of gifs on click events. Had a hell of a time.

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -4,39 +4,95 @@ console.log("Welcome to the Giftastic Gif generator!");
 // Global Variables
 
 // Starting Gifs
-var startingGifs = ["Baseball", "Football", "Basketball", "Hockey", "Cricket", "Rugby", "Wrestling", "Badminton", "Table Tennis", "Longboarding"];
-var submittedGifs = [];
+var buttonWords = ["Baseball", "Football", "Basketball", "Hockey", "Cricket", "Rugby", "Wrestling", "Badminton", "Table Tennis", "Longboarding"];
 var APIKey = "dHi75xUi9DvdwyvQZjjGM9I9GU5t17KM";
-var queryURL = "https://api.giphy.com/v1/gifs/search";
 var buttonGatheringSpace = $("#buttonGatheringSpace");
 var gifGatheringSpace = $("#gifGatheringSpace");
 
-console.log("Here are the starting button terms: " + startingGifs);
-console.log("Here are the submitted button terms: " + submittedGifs);
+console.log("Here are the  button terms: " + buttonWords);
 
-
-// Rendering Buttons
+// Rendering Buttons Function (called on later)
 function renderButtons() {
     buttonGatheringSpace.empty();
 
-    for (i = 0; i < startingGifs.length; i++) {
+    for (i = 0; i < buttonWords.length; i++) {
         var button = $("<button>");
-        button.attr("data-search", startingGifs[i]);
-        button.text(startingGifs[i]);
-        button.addClass("btn btn-primary");
-        buttonGatheringSpace.append(button);
-    };
-
-    if (submittedGifs.length > 1) {
-        var button = $("<button>");
-        button.attr("data-search", submittedGifs[i]);
-        button.text(submittedGifs[i]);
-        button.addClass("btn btn-primary");
+        button.attr("data-search", buttonWords[i]);
+        button.text(buttonWords[i]);
+        button.addClass("btn btn-primary mr-1 mb-1 clickable-search-button");
         buttonGatheringSpace.append(button);
     };
 };
 
-// API Call and Response
+// Function specifying that when any of the gif topic buttons are clicked, the values in the buttons will be queried against the GIPHY API
+$(document).on("click", ".clickable-search-button", function (event) {
+    gifGatheringSpace.empty();
+    event.preventDefault();
+    var searchedWord = $(this).attr("data-search");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchedWord + "&api_key=" + APIKey + "&limit=10";
+    console.log("Clicked on: " + searchedWord);
+
+    // Querying the GIPHY API
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+
+        // Setting response to its own variable
+        var buttonResults = response.data;
+
+        // Needing to step through each gif in the response data
+        for (i = 0; i < buttonResults.length; i++) {
+            var gifDiv = $("<div>");
+            var gifImg = $("<img>");
+            var rating = $("<h3 class='rating'>").text("Rating: " + buttonResults[i].rating);
+            gifImg.attr("src", buttonResults[i].images.fixed_height_still.url);
+            gifImg.attr("data-still", buttonResults[i].images.fixed_height_still.url);
+            gifImg.attr("data-animate", buttonResults[i].images.fixed_height.url);
+            gifImg.attr("data-state", "still");
+            gifImg.addClass("stateChange");
+            rating.attr("src", buttonResults[i].images.rating);
+            gifDiv.addClass("gif");
+            gifDiv.prepend(rating);
+            gifDiv.append(gifImg);
+            gifGatheringSpace.append(gifDiv);
+        }
+    });
+});
+
+
+// This is the function that captures the data inputted by the user.
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    var searchTerm = $("#userGifInput").val().trim();
+
+    console.log("The term that was searched: " + searchTerm);
+
+    buttonWords.push(searchTerm);
+
+    $("#userGifInput").val("");
+    renderButtons();
+});
+
+// Function allowing gifs to change state on click
+$(document).on("click", '.stateChange', function () {
+    console.log("Clicked on a gif to change its state!");
+    var state = $(this).attr("data-state");
+
+    if (state == "still") {
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+    };
+
+
+});
+
+// Making sure all buttons are rendered
+renderButtons();
 
 
 
